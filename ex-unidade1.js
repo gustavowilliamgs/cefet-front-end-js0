c90ca5838cac995879e9df59ef60a8216cd31bac
Gustavo William Gonçalves Silva <gustavowilliamgs1@gmail.com>
2024-05-31 18:12:59 +0000

    Update

--- ex-unidade1.js
+++ ex-unidade1.js
@@ -22,50 +22,56 @@ function testeSaidas() {
     //      ^ nº exerc. ^ rótulo (descrição)     
 
     // escrever também (i) b, (ii) soma de a e b, (iii) d, (iv) e   (≃ 4 linhas)
+    escreva(0.1, "Variável b", b);
+    escreva(0.1, "Soma de a e b", a + b);
+    escreva(0.1, "Variável d", d);
+    escreva(0.1, "Variável e", e);
 
-
-
-
+    escrevaMensagem(0.1, "Meu nome!")
 }
 
 // invocar testeSaidas e ver o resultado na página                  (≃ 1 linha)
-
+testeSaidas();
 
 
 
 
 // ----------------------------------------------------------------------------
 // Exercício 0.2: função soma                                    (≃ 4-5 linhas)
-
-
-
-
-
+function soma(a, b) {
+    escreva(0.2, "soma", a + b);
+    return a + b;
+}
 
 
 // ----------------------------------------------------------------------------
 // Exercício 0.3: fazendo cálculos matemáticos                  (≃ 7-10 linhas)
-
-
-
-
+function calculaValoresDoNumero(numero) {
+    escreva(0.3, "quadrado", numero * numero);
+    escreva(0.3, "metade", numero / 2);
+    escreva(0.3, "raiz", Math.sqrt(numero));
+}
 
 
 
 // ----------------------------------------------------------------------------
 // Exercício 1: altura da bola no instante                      (≃ 5-10 linhas)
 function calculaAlturaBola(tempo, velocidadeInicial, gravidade) {
-    let altura = null;
+    let altura = (velocidadeInicial * tempo) - ((gravidade * (tempo * tempo)) / 2);
     return altura;
 }
 
-
-
-
+escreva(1, "Teste 1", calculaAlturaBola(1, 50, 9.81));
+escreva(1, "Teste 2", calculaAlturaBola(5, 50, 9.81));
+escreva(1, "Teste 3", calculaAlturaBola(10, 50, 9.81));
 
 // ----------------------------------------------------------------------------
 // Exercício 2: velocidade da bola no instante                  (≃ 6-12 linhas)
+function calculaVelocidadeBola (tempo, velocidadeInicial, gravidade) {
+    let velocidade = velocidadeInicial - (gravidade * tempo); 
+    return velocidade;
+}
 
-
-
-
+escreva(2, "Teste 1", calculaVelocidadeBola(1, 50, 9.81));
+escreva(2, "Teste 2", calculaVelocidadeBola(5, 50, 9.81));
+escreva(2, "Teste 3", calculaVelocidadeBola(10, 50, 9.81));
--- ex-unidade2.js
+++ ex-unidade2.js
@@ -1,15 +1,19 @@
 function contandoElementosDoVetor(elementos, elementoSendoProcurado) {
     let quantidade = 0;
     for (let elemento of elementos) {
-        
-        
+        if (elemento === elementoSendoProcurado) {
+            quantidade++;
+        }
     }
     
     return quantidade;
 }
 
+escreva(0.4, "O elemento sendo procurado apareceu ", contandoElementosDoVetor([1, 5, 5], 5) + " vezes");                   // retorna 2
+escreva(0.4, "O elemento sendo procurado apareceu ", contandoElementosDoVetor(['daniel', 'flávio'], 'joão') + " vezes");   // retorna 0
+
 function obtemNomeDoMes(numero) {
-    if (numero < null || numero > null) {
+    if (numero < 1 || numero > 12) {
         escrevaMensagem(0.5, 'Mês inválido: ' + numero);
         return null;
     }
@@ -29,18 +33,76 @@ function obtemNomeDoMes(numero) {
         'dezembro'
     ];
 
-    return meses[null];
+    return meses[numero - 1];
 }
 
+escreva(0.5, "Teste 1", obtemNomeDoMes(4));
+escreva(0.5, "Teste 2", obtemNomeDoMes(9));
+
+function calculaVelocidadeAlturaBola (velocidadeInicial, gravidade, n) {
+    for (let i = 1; i <= n; i++) {
+        escreva(3, `h(${i})`, calculaAlturaBola(i, velocidadeInicial, gravidade));
+        escreva(3, `v(${i})`, calculaVelocidadeBola(i, velocidadeInicial, gravidade));
+    }
+}
+
+calculaVelocidadeAlturaBola(50, 9.81, 20);
+
+function somatorio (n) {
+    let s = 0;
+
+    for (let i = 2; i <= n; i += 2) {
+        s += 1 / i;
+    }
+
+    return s;
+}
+
+escreva(4, "Somatório", somatorio(1));
+escreva(4, "Somatório", somatorio(10));
+escreva(4, "Somatório", somatorio(100));
 
 
+let frutas = ['Pera', 'Uva', 'Abacaxi', 'Cenoura']
 
+function obtemPosicaoDoElemento (vetor, elemento) {
+    let posicao = vetor.indexOf(elemento);
+    return posicao != -1 ? posicao : null;
+}
+
+escreva(5, "Teste 1", obtemPosicaoDoElemento(frutas, 'Abacaxi'));
+
+
+function calculaMediaEntreExtremos(vetor) {
+    let max = vetor[0];
+    let min = vetor[0];
+
+    for (const elemento of vetor) {
+        if (elemento > max) {
+            max = elemento;
+        } else if (elemento < min) {
+            min = elemento;
+        }
+    }
+
+    return (min + max) / 2;
+}
+
+escreva(6, "Teste 1", calculaMediaEntreExtremos([3, -2, 12]));
 
 function fibonacci(tamanhoSequencia) {
-    let sequencia = [];
+    if (tamanhoSequencia <= 0) return [];
+    if (tamanhoSequencia === 1) return [0];
     
+    let sequencia = [0, 1];
+    
+    for (let i = 2; i < tamanhoSequencia; i++) {
+        sequencia.push(sequencia[i - 2] + sequencia[i - 1]);
+    }
     
     escreva(7, 'Fib(' + tamanhoSequencia + ')', sequencia);
+
     return sequencia;
 }
 
+fibonacci(7);
